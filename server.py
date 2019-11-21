6cbf8365f8d304a2aafa9c07daa8bf45a2ff32e7
Willy <willychenwii@gmail.com>
2019-11-21 22:32:17 +0800

    Update

--- server.py
+++ server.py
@@ -39,7 +39,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     userinput = event.message.text
-    if userinput.find('救我'):
+    if userinput.find('救我')!=-1:
         line_bot_api.reply_message(event.reply_token,TextSendMessage('我救不了你'))
 
 
